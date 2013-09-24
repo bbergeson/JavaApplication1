6c093bd86442396f72cdcd608b7558d239b71f43
javaoneholmachine <training+javaoneholmachine@github.com>
2013-09-23 17:02:45 -0700

    Printed a message

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,5 +15,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Thi is a test.");
     }
 }
